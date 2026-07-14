2e2d906ac2bff16160a340d2d49a74199a685039
farooqwrites <farooqsatta@gmail.com>
2026-07-14 22:00:27 +0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,35 @@
+// ===============================
+// Farooq Writes Firebase
+// ===============================
 
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
+
+import {
+  getFirestore
+} from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
+
+import {
+  getAnalytics
+} from "https://www.gstatic.com/firebasejs/12.16.0/firebase-analytics.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBaWAkE-RT2BROPAnxXINTgR-2SBR5r_I8",
+  authDomain: "farooq-writes.firebaseapp.com",
+  projectId: "farooq-writes",
+  storageBucket: "farooq-writes.firebasestorage.app",
+  messagingSenderId: "968386759160",
+  appId: "1:968386759160:web:d095bc9d505d098b6eee0d",
+  measurementId: "G-Z5PPK164B1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Firestore Database
+const db = getFirestore(app);
+
+// Analytics
+const analytics = getAnalytics(app);
+
+// Export
+export { db };
