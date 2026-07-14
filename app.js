5cb7c24dca6a6b28dcc1370630c293cb81c97de1
farooqwrites <farooqsatta@gmail.com>
2026-07-14 23:33:44 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,40 @@
+import {
+  db,
+  doc,
+  getDoc,
+  setDoc,
+  updateDoc,
+  increment
+} from "./firebase.js";
 
+const counter = document.getElementById("visitor-count");
+
+async function updateVisitors() {
+  try {
+    const ref = doc(db, "website", "stats");
+
+    const snap = await getDoc(ref);
+
+    if (!snap.exists()) {
+      await setDoc(ref, {
+        visitors: 1
+      });
+
+      counter.textContent = 1;
+      return;
+    }
+
+    await updateDoc(ref, {
+      visitors: increment(1)
+    });
+
+    const updated = await getDoc(ref);
+
+    counter.textContent = updated.data().visitors;
+
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+updateVisitors();
