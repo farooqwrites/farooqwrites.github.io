841f09d59d16ae1c85c5530cbd1cfdfe7193749c
farooqwrites <farooqsatta@gmail.com>
2026-07-14 23:30:54 +0500

    Refactor Firebase imports and exports
    Updated Firebase imports and exports to include Firestore functions.

--- firebase.js
+++ firebase.js
@@ -1,13 +1,15 @@
-// ===============================
-// Farooq Writes Firebase
-// Final Version
-// ===============================
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
 
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
-
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-analytics.js";
+
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc,
+  updateDoc,
+  increment
+} from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBaWAkE-RT2BROPAnxXINTgR-2SBR5r_I8",
@@ -19,14 +21,18 @@ const firebaseConfig = {
   measurementId: "G-Z5PPK164B1"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Analytics
 const analytics = getAnalytics(app);
 
-// Firestore
 const db = getFirestore(app);
 
-// Export for app.js
-export { app, db, analytics };
+export {
+  db,
+  doc,
+  getDoc,
+  setDoc,
+  updateDoc,
+  increment
+};
+
